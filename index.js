0f0b56e0e293f5f012b28de854c04753d33a6dcb
Captain Fantasy <captain@fantasyui.com>
2017-07-24 13:56:09 -0400

    Upgrade Class Selector

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+const select = require('postcss-select')
+
 module.exports = function({theme, type}){
 
     let cssFilePath = `./dist/${theme}.css`;
@@ -5,9 +11,13 @@ module.exports = function({theme, type}){
 
     // EXTRACT DECLARATIONS FOR cssSelector FROM cssFilePath AND RETURN AS STRING
 
-    return `
-      border: 1px solid orange;
-      background-color: red;
-    `;
+    let myCss = fs.readFileSync(cssFilePath);
+    let response = postcss([select([cssSelector])]).process(myCss).css
+    console.log(response);
+    return response;
+    // return `
+    //   border: 1px solid orange;
+    //   background-color: red;
+    // `;
 
 }
