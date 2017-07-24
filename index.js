2174cc9b0324f0aaf1655a9c728492e9a7b653e4
Captain Fantasy <captain@fantasyui.com>
2017-07-24 13:45:29 -0400

    Initial Theme Generator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function({theme, type}){
 
-    let cssFilePath = `./themes/${theme}`;
+    let cssFilePath = `./dist/${theme}.css`;
     let cssSelector = `.${type}`;
 
     // EXTRACT DECLARATIONS FOR cssSelector FROM cssFilePath AND RETURN AS STRING
