8ea91c19ea556406f095b65fbd1dcd0eca5ae428
Captain Fantasy <captain@fantasyui.com>
2017-07-25 15:59:41 -0400

    Add styleguide / html demo files (/dist)

--- build.js
+++ build.js
@@ -7,7 +7,15 @@ const colorFunction = require("postcss-color-function")
 
 fs.readdir( path.join(__dirname, 'src', 'lib'), (err, files) => {
   files.forEach(fileName=>{
-    fs.readFile(`src/main.css`, (err, css) => {
+
+    const name = path.basename(fileName, '.css');
+
+    fs.readFile(`src/guide.html`, (err, html) => {
+      html = html.toString().replace(/<%= theme %>/g, name);
+      fs.writeFile(`dist/${name}.html`, html);
+    });
+
+    fs.readFile(`src/style.css`, (err, css) => {
       css = `@import "./lib/${fileName}";\n\n` + css;
         postcss([ precss, colorFunction, autoprefixer ])
         .process(css, { from: `src/${fileName}` /* <- pretend filename */, to: `dist/${fileName}` })
@@ -16,5 +24,6 @@ fs.readdir( path.join(__dirname, 'src', 'lib'), (err, files) => {
             if ( result.map ) fs.writeFile(`dist/${fileName}.map`, result.map);
         }).catch(err => console.error(err) );
     });
+
   })
 });
