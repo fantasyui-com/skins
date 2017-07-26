ef54ca799a2548e25da71789c9deb71058264603
Captain Fantasy <captain@fantasyui.com>
2017-07-26 18:57:47 -0400

    Add general purpose spacers.

--- build.js
+++ build.js
@@ -5,6 +5,8 @@ const precss = require('precss');
 const autoprefixer = require('autoprefixer');
 const colorFunction = require("postcss-color-function")
 
+const spacers = require("spacers");
+
 fs.readdir( path.join(__dirname, 'src', 'lib'), (err, files) => {
   files.forEach(fileName=>{
 
@@ -14,6 +16,8 @@ fs.readdir( path.join(__dirname, 'src', 'lib'), (err, files) => {
       html = html.toString().replace(/<%= theme %>/g, name);
       fs.writeFile(`dist/${name}.html`, html);
     });
+
+    spacers.writeFile(`dist/spacers.css`);
 
     fs.readFile(`src/style.css`, (err, css) => {
       css = `@import "./lib/${fileName}";\n\n` + css;
